4cf0ff1bfe2505e02294346dd48bc0d508672b0b
gplesz <gabor.plesz@gmail.com>
2017-10-10 14:24:45 +0200

    #5: Category létrehozása

---
+++ OopRestaurant201710/OopRestaurant201710/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace OopRestaurant201710
+{
+    /// <summary>
+    /// Az ételek kategóriáját tartalmazó osztály
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Az adatbázisba írás miatt kell Primary Key (PK) a táblába
+        /// ezt hozza létre ez a property. Mivel Id a neve a Code first 
+        /// automatikusan tudja, hogy ő PK
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Az étel kategória neve
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
