26d8efbdcca83eb18aeeb6f56ba59f33057f9fdf
gplesz <gabor.plesz@gmail.com>
2017-10-24 14:35:26 +0200

    #5, #6: A két új táblát feltöltöttük tesztadatokkal.

--- OopRestaurant201710/OopRestaurant201710/Migrations/Configuration.cs
+++ OopRestaurant201710/OopRestaurant201710/Migrations/Configuration.cs
@@ -23,13 +23,36 @@ namespace OopRestaurant201710.Migrations
             context.Categories.AddOrUpdate(x => x.Name, new Category(name: "Desszertek"));
             context.SaveChanges();
 
-            var pizzaCategory = context.Categories.Single(x => x.Name == "Pizzák");
+            //LINQ
+            var pizzaCategory = context.Categories
+                                       .Single(x => x.Name == "Pizzák"); //azt az egyetlent keressük, amire ez igaz. Ha nincs: hiba, ha több van: hiba
 
             context.MenuItems.AddOrUpdate(x => x.Name, new MenuItem(name: "Margarita",
                 description: "Mozzarella sajt, paradicsomszósz", price: 100, category: pizzaCategory));
 
             context.MenuItems.AddOrUpdate(x => x.Name, new MenuItem(name: "Hawaii",
                 description: "Sonka, ananász, mozzarella sajt, paradicsomszósz", price: 100, category: pizzaCategory));
+            context.SaveChanges();
+
+
+            //Helyiségek feltöltése
+            context.Locations.AddOrUpdate(x => x.Name, new Location() { Name = "Terasz", IsOutdoor = true }); //ha nem lenne megfelelõ konstruktor, így mindig használható 
+            context.Locations.AddOrUpdate(x => x.Name, new Location("Belsõ terem", false)); //de az elõzõ ugyanaz csinálja mint ez, ha van ilyen konstruktor
+            context.SaveChanges();
+
+            var outdoorLocation = context.Locations
+                                         .Where(x => x.Name == "Terasz") //Az összes sort visszaadja, amire ez igaz. Ha nincs: üres lista, ha több van: hosszabb lista
+                                         //.Single(); ha ezt írnánk, akkor ugyanazt írnánk mint a pizzáknál
+                                         //.First(); ha üres a lista: hiba, ha van elem a listán, akkor az elsõ elemt adja vissza.
+                                         .FirstOrDefault(); //Ha üres a lista: null-t ad vissza, ha van elem a listán, akkor az elsp elemet adja
+
+            if (outdoorLocation == null)
+            { //ha nincs location példány nem érdemes továbbmenni
+                throw new Exception("Nincs megfelelõ Location az adatbázisban");
+            }
+
+            //Asztalok feltöltése
+            context.Tables.AddOrUpdate(x => x.Name, new Table() { Name = "Bal-2", Location = outdoorLocation });
             context.SaveChanges();
         }
     }
--- OopRestaurant201710/OopRestaurant201710/Models/Location.cs
+++ OopRestaurant201710/OopRestaurant201710/Models/Location.cs
@@ -9,6 +9,23 @@ namespace OopRestaurant201710.Models
     public class Location
     {
         /// <summary>
+        /// Alapértelmezett paraméter nélküli konstruktor az 
+        /// EntityFramework-nek
+        /// </summary>
+        public Location() { }
+
+        /// <summary>
+        /// Az a konstruktor, ami a használhatóságot javítja
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="isOutdoor"></param>
+        public Location(string name, bool isOutdoor)
+        {
+            Name = name;
+            IsOutdoor = isOutdoor;
+        }
+
+        /// <summary>
         /// PK mező az adatbázishoz
         /// </summary>
         public int Id { get; set; }
